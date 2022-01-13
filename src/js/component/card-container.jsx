71f4f9177c52ed0ca5d41c58977be203417263ea
MaiCodes-exe <maise.b.costa@gmail.com>
2022-01-13 15:43:16 +0000

    Fixed the CardContainer structure

--- src/js/component/card-container.jsx
+++ src/js/component/card-container.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 import Card from "../component/card.jsx";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function CardContainer() {
 	return (
 		<>
-			<Card />
+			<Container>
+				<Row>
+					<Col sm={12} md={3}>
+						<Card></Card>
+					</Col>
+					<Col sm={12} md={3}>
+						<Card></Card>
+					</Col>{" "}
+					<Col sm={12} md={3}>
+						<Card></Card>
+					</Col>{" "}
+					<Col sm={12} md={3}>
+						<Card></Card>
+					</Col>
+				</Row>
+				<Row>
+					<Col sm>sm=true</Col>
+					<Col sm>sm=true</Col>
+					<Col sm>sm=true</Col>
+				</Row>
+			</Container>
 		</>
 	);
 }
